954807144a364859a03a1b5685cf08d7af5e1333
IsaiasTX21 <isaiasnovaera@gmail.com>
2025-02-11 17:18:11 -0300

    2025 aa

--- src/body.jsx
+++ src/body.jsx
@@ -14,12 +14,12 @@ function Body() {
           <p style={{ fontSize: "18px" }} className=" text-cgenter">Me chamo Isaias e gostaria que voce enxergasse
             o meu mundo como eu vejo. Minha paixão por tecnologia faz meu coração acelerar e superar meus limites. </p>
 
-          <img style={{ height: "300px", left: "100px" }} src="src/photos/Captura de tela 2025-02-09 23023.png" className=" fadephoto  float-end position-relative"  />
+          <img style={{ height: "300px", left: "100px" }} src="./Captura de tela 2025-02-09 23023.png" className=" fadephoto  float-end position-relative"  />
           <div style={{ bottom: "180px" }} className="  d-flex position-relative justify-content-center w-100 " >
             <button className="  btn btn-dark text-center">currículo</button>  </div>
         </div>
 
-        <div style={{ height: "95vh", backgroundImage: "url('src/photos/Captura de tela 2025-02-10 194259.png')", backgroundSize: "cover" }} className=" col-md-5 "></div>
+        <div style={{ height: "95vh", backgroundImage: "url('src/Captura de tela 2025-02-10 194259.png')", backgroundSize: "cover" }} className=" col-md-5 "></div>
 
       </div>
 
@@ -32,11 +32,11 @@ function Body() {
 
         <div style={{ height: "20vh", backgroundColor: "black" }} className=" d-flex justify-content-center align-items-end ">
 
-          <img style={{ top: "100px", height: "200px" }} src="src/photos/Captura de tela 2025-02-09 23023.png" className=" fadephotomobile position-relative d-flex justify-content-center align-items-end " />
+          <img style={{ top: "100px", height: "200px" }} src="Captura de tela 2025-02-09 23023.png" className=" fadephotomobile position-relative d-flex justify-content-center align-items-end " />
         </div>
 
 
-        <div style={{ backgroundImage: "url('src/photos/Captura de tela 2025-02-10 201835.png')", backgroundSize: "cover", height: "50vh" }} className=" col-md-5 ">  <div style={{ top: "180px" }} className="d-flex position-relative  justify-content-center " >
+        <div style={{ backgroundImage: "url('Captura de tela 2025-02-10 201835.png')", backgroundSize: "cover", height: "50vh" }} className=" col-md-5 ">  <div style={{ top: "180px" }} className="d-flex position-relative  justify-content-center " >
           <button className="btn btn-dark text-center">currículo</button>  </div></div>
       </div>
 
