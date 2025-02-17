1f63d4404a848c6a0cbd08adb84bb57d23418553
IsaiasTX21 <isaiasnovaera@gmail.com>
2025-02-16 22:56:09 -0300

    portifolio

--- src/body.jsx
+++ src/body.jsx
@@ -6,15 +6,21 @@ function Body() {
   return (
     <>
       <Header />
-      <div className=" d-md-flex desk  ">
+     
+      <div className=" d-md-flex desk flex-column  ">
+       
+        <main style={{ height: "95vh", backgroundColor: "#01011A", backgroundRepeat:"no-repeat", backgroundSize:"100vh", backgroundImage:"url('planet-earth-1457453_1280-Photoroom.jpg')", backgroundPositionY:"100%", backgroundPositionX:"50%" }} className=" d-flex flex-column text-center  col-md-12    ">
+      
+          <h1 style={{ color: "#6F848C", marginTop: "10vh" }} className="text-center fade "> Hello world !</h1>
+        
 
-        <main style={{ height: "95vh", backgroundColor: "#01011A" }} className=" d-flex flex-column text-center  col-md-12    ">
-          <h1 style={{ color: "#6F848C", marginTop: "25vh" }} className="text-center fade "> Hello world !</h1>
-          <p style={{ fontSize: "18px", color: "#6F848C" }} className=" w-50 mt-4 fade ms-auto me-auto ">Me chamo Isaias e gostaria que voce enxergasse
+          <p style={{ fontSize: "18px", color: "#6F848C" }} className=" w-50 mt-4 fade ms-auto me-auto ">Me chamo Isaias e gostaria que você
+          enxergasse
             o meu mundo como eu vejo. Minha paixão por tecnologia faz meu coração acelerar e superar meus limites. </p>
           <a href="https://drive.google.com/file/d/1IKLGIUwCZTcSwgU5HRqQz_VP95eqVAC4/view?usp=drive_link" className="fade btn btn-primary ms-auto me-auto mt-4  text-white " >currículo</a>
+         
         </main>
-        
+      
       </div>
 
       <div className="  d-md-none mobile d-flex flex-column   ">
