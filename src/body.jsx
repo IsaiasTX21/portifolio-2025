01d159b76f0c1915bd1b271f650e0c40870380a8
IsaiasTX21 <isaiasnovaera@gmail.com>
2025-02-16 23:05:03 -0300

    portifolio

--- src/body.jsx
+++ src/body.jsx
@@ -9,7 +9,7 @@ function Body() {
      
       <div className=" d-md-flex desk flex-column  ">
        
-        <main style={{ height: "95vh", backgroundColor: "#01011A", backgroundRepeat:"no-repeat", backgroundSize:"100vh", backgroundImage:"url('planet-earth-1457453_1280-Photoroom.jpg')", backgroundPositionY:"100%", backgroundPositionX:"50%" }} className=" d-flex flex-column text-center  col-md-12    ">
+        <main style={{ height: "95vh", backgroundColor: "#01011A", backgroundRepeat:"no-repeat", backgroundSize:"100vh", backgroundImage:"url('https://i.ibb.co/HLC6rDTz/planet-earth-1457453-1280-Photoroom.jpg')", backgroundPositionY:"100%", backgroundPositionX:"50%" }} className=" d-flex flex-column text-center  col-md-12    ">
       
           <h1 style={{ color: "#6F848C", marginTop: "10vh" }} className="text-center fade "> Hello world !</h1>
         
@@ -25,9 +25,9 @@ function Body() {
 
       <div className="  d-md-none mobile d-flex flex-column   ">
 
-        <main style={{ backgroundColor: "#01011A", height: "95vh" }} className=" text-center"  >
+        <main style={{ height: "95vh", backgroundColor: "#01011A", backgroundRepeat:"no-repeat", backgroundSize:"100vh", backgroundImage:"url('https://i.ibb.co/HLC6rDTz/planet-earth-1457453-1280-Photoroom.jpg')", backgroundPositionY:"100%", backgroundPositionX:"50%" }} className=" text-center"  >
 
-          <h1 className="fade" style={{ color: "#6F848C", marginTop: "25vh" }} > Hello world !</h1>
+          <h1 className="fade" style={{ color: "#6F848C", marginTop: "10vh" }} > Hello world !</h1>
           <p style={{ fontSize: "18px", color: " #6F848C " }} className="fade mt-5 ">Me chamo isaias e gostaria que voce enchergasse
             o meu mundo como eu vejo. Minha paixão por tecnologia faz meu coração acelerar e superar meus limites. </p>
           <a style={{ position: "relative" }} href="https://drive.google.com/file/d/1IKLGIUwCZTcSwgU5HRqQz_VP95eqVAC4/view?usp=drive_link" className=" mt-5 fade btn btn-primary  text-white " >currículo</a>
